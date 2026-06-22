80b5fdba22cdeb0272edb390ad4a9db5664acff8
Benard <snipedollarfx@gmail.com>
2026-06-22 13:59:38 +0300

    Fix Express 5 wildcard route error

--- server/index.js
+++ server/index.js
@@ -101,7 +101,7 @@ app.post('/api/withdraw', (req, res) => {
 
 // Serve frontend static files
 app.use(express.static(path.join(__dirname, '../dist')));
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
